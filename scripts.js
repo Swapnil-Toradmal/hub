bdc2df8ca983944808ac240a33c11c5fc655d2ae
Swapnil Toradmal <102501912+Swapnil-Toradmal@users.noreply.github.com>
2025-02-23 13:58:46 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 function sendToWhatsApp() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
+            var name = document.getElementById("name").value;
+            var email = document.getElementById("email").value;
+            var message = document.getElementById("message").value;
+			if (!name || !email || !message) {
+				alert("Please fill out all fields.");
+				event.preventDefault();
+			} else {
+				alert("Message sent successfully!");
+			}
 
-    if (!name || !email || !message) {
-        alert("Please fill out all fields.");
-        event.preventDefault();
-    } else {
-        alert("Message sent successfully!");
-    }
+            var whatsappURL = "https://wa.link/ijezpz";
+            var text = "Hello! My Name: " + name + "%0AEmail: " + email + "%0AMessage: " + message;
 
-    var whatsappURL = "https://wa.link/ijezpz"; // Updated WhatsApp link
-    var text = "Hello! My Name: " + name + "%0AEmail: " + email + "%0AMessage: " + message;
-
-    // Open WhatsApp with the pre-filled message
-    window.open("https://wa.me/919975757365?text=" + text, "_blank");
-}
+            // Open WhatsApp with the pre-filled message
+            window.open("https://wa.me/919975757365?text=" + text, "_blank");
+        }
+;
